33d202efe60b69a54135af5429704fcc18b3b653
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 18:46:14 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,6 +49,6 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-  spice_track = matrix[row][column]
-  new_spicetrack
+  spice_rack = matrix[row][column]
+  spice_rack
 end
